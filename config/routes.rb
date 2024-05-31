94bfcf4a384acbf990846aa46f3eafc531257245
eliezer.moraesss <eliezer.moraes@outlook.com>
2024-05-31 15:24:17 -0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # config/routes.rb
 Rails.application.routes.draw do
+  # Definindo a rota raiz ("/")
   root 'game#index'
+
+  # Rota para checar o status de saúde da aplicação
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Rotas do controlador 'game'
   post 'guess', to: 'game#guess'
   post 'reset', to: 'game#reset'
 end
